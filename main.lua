935365b3a6ff3dfadde3f07042267b83aa94f115
Ray <raymodsgames@gmail.com>
2018-11-21 01:03:23 -0800

    Add intro anim

--- main.lua
+++ main.lua
@@ -3,44 +3,64 @@
     X: 403.9289
     Y: -996.6722
     Z: -100
+
+  Customise Anim Start:
+    X: 405.62100
+    Y: -997.099365
+    Z: -100
+    Rotation: 85
 ]]--
 
+local FEMALE_ANIM = 'mp_character_creation@customise@female_a';
 local FEMALE_MODEL = GetHashKey('mp_f_freemode_01')
+local MALE_ANIM = 'mp_character_creation@customise@male_a';
 local MALE_MODEL = GetHashKey('mp_m_freemode_01')
 
 
-local dictName = 'mp_character_creation@lineup@male_a'
 Citizen.CreateThread(function ()
-  RequestModel(MALE_MODEL)
-  while (not HasModelLoaded(MALE_MODEL)) do
+  setPlayerModel(MALE_MODEL)
+  playIntroAnim(MALE_ANIM)
+end)
+
+function playIntroAnim(animKey)
+  RequestAnimDict(animKey)
+  while (not HasAnimDictLoaded(animKey)) do
     Citizen.Wait(100)
   end
 
-  SetPlayerModel(PlayerId(), MALE_MODEL)
-  SetPedDefaultComponentVariation(PlayerPedId())
+  StartPlayerTeleport(PlayerId(), 405.621, -997.099365, -100.0, 92.0)
+  while (IsPlayerTeleportActive()) do
+    Citizen.Wait(5)
+  end
+  TaskPlayAnim(PlayerPedId(), animKey, 'intro', 1.0, -1.0, 5000, 0, 1, true, true, true)
+  Citizen.Wait(5000)
+  -- TODO: End this when customization is done
+  local times = 0
+  while times < 1 do
+    TaskPlayAnim(PlayerPedId(), animKey, 'drop_intro', 1.0, -1.0, 5000, 0, 2, true, true, true)
+    times = times + 1
+    Citizen.Wait(5000)
+  end
 
-  --[[
-    Play anims to imitate GTA:O opening anim into mugshot:
+end
 
-    RequestAnimDict('mp_character_creation@lineup@male_a')
-    while (not HasAnimDictLoaded('mp_character_creation@lineup@male_a')) do
+function setPlayerModel(modelKey)
+  if(GetEntityModel(PlayerPedId()) ~= modelKey) then
+    RequestModel(modelKey)
+    while (not HasModelLoaded(modelKey)) do
       Citizen.Wait(100)
     end
 
-    TaskPlayAnim(PlayerPedId(), 'mp_character_creation@lineup@male_a', 'intro', 1.0, -1.0, 5000, 0, 1, true, true, true)
-    Citizen.Wait(5000)
-    TaskPlayAnim(PlayerPedId(), 'mp_character_creation@lineup@male_a', 'intro', 1.0, -1.0, 5000, 0, 1, true, true, true);
-
-    TODO: Sync up the two animations above so the first results in ped facing correct direction when
-    second plays. Might require updating ped's coords between animations. Also need to set the ped's
-    start coords outside the mugshot room to walk in.
-  ]]--
-end)
+    SetPlayerModel(PlayerId(), modelKey)
+    SetPedDefaultComponentVariation(PlayerPedId())
+  end
+end
 
 Citizen.CreateThread(function ()
   while true do
     Citizen.Wait(0)
     local playerCoords = GetEntityCoords(PlayerPedId())
+    local heading = GetEntityHeading(PlayerPedId())
 
     SetTextScale(0, 0.25)
     SetTextDropShadow(1, 0, 0, 0, 255)
@@ -50,7 +70,7 @@ Citizen.CreateThread(function ()
     SetTextScale(0, 0.25)
     SetTextDropShadow(1, 0, 0, 0, 255)
     SetTextEntry("STRING")
-    AddTextComponentString('x: ' .. playerCoords.x .. ' y: ' .. playerCoords.y .. ' z: ' .. playerCoords.z)
+    AddTextComponentString('x: ' .. playerCoords.x .. ' y: ' .. playerCoords.y .. ' z: ' .. playerCoords.z .. ' head: ' .. heading)
     DrawText(0.01, 0.375)
     SetTextScale(0, 0.25)
     SetTextDropShadow(1, 0, 0, 0, 255)
