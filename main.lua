807dfce6266ec2d4bf416c037618ec6649ba77a8
Ray <raymodsgames@gmail.com>
2018-11-26 00:07:38 -0800

    Add initial intro camera implementation

--- main.lua
+++ main.lua
@@ -16,8 +16,53 @@ local FEMALE_MODEL = GetHashKey('mp_f_freemode_01')
 local MALE_ANIM = 'mp_character_creation@customise@male_a';
 local MALE_MODEL = GetHashKey('mp_m_freemode_01')
 
-
 Citizen.CreateThread(function ()
+  DoScreenFadeOut()
+  Citizen.Wait(500)
+
+  DoScreenFadeIn(7000)
+end)
+
+
+function createCam(x, y, z)
+  return CreateCameraWithParams('DEFAULT_SCRIPTED_CAMERA', x, y, z, 0, 0, 0, 60.0, true, 2)
+end
+
+-- Camera Anim Thread
+Citizen.CreateThread(function ()
+  Citizen.Wait(500)
+
+  local startCoord = {
+    x = 402.0,
+    y = -1002.2399,
+    z = -99,
+  }
+  local endCoord = {
+    x = 402.8,
+    y = -999.0,
+    z = -99,
+  }
+
+
+  local cam1 = createCam(402.8, -1002.2399, -99.05)
+  local cam2 = createCam(402.8, -999.125, -99.05)
+  local cam3 = createCam(402.8, -999.1, -99.05)
+
+  while (not DoesCamExist(cam1) and not DoesCamExist(cam2) and not DoesCamExist(cam3)) do
+    Citizen.Wait(5)
+  end
+
+  SetCamActive(cam1, true)
+  RenderScriptCams(true, true, 0, false, false)
+  Citizen.Wait(100)
+  SetCamActiveWithInterp(cam2, cam1, 3500, 0, 0)
+  Citizen.Wait(3500)
+  SetCamActiveWithInterp(cam3, cam2, 500, 0, 0)
+end)
+
+-- Ped Anim Thread
+Citizen.CreateThread(function ()
+  Citizen.Wait(0)
   setPlayerModel(MALE_MODEL)
   playIntroAnim(MALE_ANIM)
 end)
@@ -37,7 +82,7 @@ function playIntroAnim(animKey)
   -- TODO: End this when customization is done
   local times = 0
   while times < 1 do
-    TaskPlayAnim(PlayerPedId(), animKey, 'drop_intro', 1.0, -1.0, 5000, 0, 2, true, true, true)
+    TaskPlayAnim(PlayerPedId(), animKey, 'loop', 1.0, -1.0, 5000, 0, 2, true, true, true)
     times = times + 1
     Citizen.Wait(5000)
   end
