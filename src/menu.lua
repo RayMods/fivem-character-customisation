bc55ad9a3c221d1d99806c96e4a8fc725746b810
Ray <raymodsgames@gmail.com>
2018-11-26 23:30:20 -0800

    features & apperance menu options

--- src/menu.lua
+++ src/menu.lua
@@ -9,7 +9,7 @@ function AddSexItem()
 end
 
 function AddHeritageMenu()
-  local heritageMenu = _pool:AddSubMenu(MainMenu, "Heritage")
+  local heritageMenu = _pool:AddSubMenu(MainMenu, 'Heritage', 'Select to choose your parents.')
   local fatherOptions = { 'Adrian', 'Alex', 'Andrew', 'Angel', 'Anthony', 'Benjamin', 'Claude', 'Daniel', 'Diego', 'Ethan', 'Evan', 'Gabriel', 'Isaac', 'John', 'Joshua', 'Juan', 'Kevin', 'Louis', 'Michael', 'Niko', 'Samual', 'Santiago', 'Vincent' }
   local motherOptions = { 'Amelia', 'Ashely', 'Audrey', 'Ava', 'Briana', 'Camila', 'Charlotte', 'Elizabeth', 'Emma', 'Evelyn', 'Giselle', 'Grace', 'Hannah', 'Isabella', 'Jasmine', 'Misty', 'Natalie', 'Nicole', 'Olivia', 'Sophie', 'Violet', 'Zoe' }
   local resemblanceAmt = {}
@@ -30,8 +30,59 @@ function AddHeritageMenu()
   heritageMenu:AddItem(skinToneItem)
 end
 
+function AddFeaturesMenu()
+  local featuresMenu = _pool:AddSubMenu(MainMenu, 'Features', 'Select to alter your facial features.')
+
+  local browItem = NativeUI.CreateListItem('Brow', { 'Custom' }, 1, 'Make changes to your physical Features.')
+  local eyesItem = NativeUI.CreateListItem('Eyes', { 'Custom' }, 1, 'Make changes to your physical Features.')
+  local noseItem = NativeUI.CreateListItem('Nose', { 'Custom' }, 1, 'Make changes to your physical Features.')
+  local noseProfileItem = NativeUI.CreateListItem('Nose Profile', { 'Custom' }, 1, 'Make changes to your physical Features.')
+  local noseTipItem = NativeUI.CreateListItem('Nose Tip', { 'Custom' }, 1, 'Make changes to your physical Features.')
+  local cheekbonesItem = NativeUI.CreateListItem('Cheekbones', { 'Custom' }, 1, 'Make changes to your physical Features.')
+  local cheeksItem = NativeUI.CreateListItem('Cheeks', { 'Custom' }, 1, 'Make changes to your physical Features.')
+  local lipsItem = NativeUI.CreateListItem('Lips', { 'Custom' }, 1, 'Make changes to your physical Features.')
+  local jawItem = NativeUI.CreateListItem('Jaw', { 'Custom' }, 1, 'Make changes to your physical Features.')
+  local chinProfileItem = NativeUI.CreateListItem('Chin Profile', { 'Custom' }, 1, 'Make changes to your physical Features.')
+  local chinShapeItem = NativeUI.CreateListItem('Chin Shape', { 'Custom' }, 1, 'Make changes to your physical Features.')
+
+  featuresMenu:AddItem(browItem)
+  featuresMenu:AddItem(eyesItem)
+  featuresMenu:AddItem(noseItem)
+  featuresMenu:AddItem(noseProfileItem)
+  featuresMenu:AddItem(noseTipItem)
+  featuresMenu:AddItem(cheekbonesItem)
+  featuresMenu:AddItem(cheeksItem)
+  featuresMenu:AddItem(lipsItem)
+  featuresMenu:AddItem(jawItem)
+  featuresMenu:AddItem(chinProfileItem)
+  featuresMenu:AddItem(chinShapeItem)
+end
+
+function addAppearanceItem(name, menu)
+  local item = NativeUI.CreateListItem(name, { 'Current' }, 1, 'Make changes to your Appearance.')
+  menu:AddItem(item)
+end
+
+function AddAppearanceMenu()
+  local featuresMenu = _pool:AddSubMenu(MainMenu, 'Appearance', 'Select to change your Appearance.')
+
+  addAppearanceItem('Hair', featuresMenu)
+  addAppearanceItem('Eyebrows', featuresMenu)
+  addAppearanceItem('Skin Blemishes', featuresMenu)
+  addAppearanceItem('Skin Aging', featuresMenu)
+  addAppearanceItem('Skin Complexion', featuresMenu)
+  addAppearanceItem('Moles & Freckles', featuresMenu)
+  addAppearanceItem('Skin Damage', featuresMenu)
+  addAppearanceItem('Eye Color', featuresMenu)
+  addAppearanceItem('Eye Makeup', featuresMenu)
+  addAppearanceItem('Blusher', featuresMenu)
+  addAppearanceItem('Lipstick', featuresMenu)
+end
+
 AddSexItem()
 AddHeritageMenu()
+AddFeaturesMenu()
+AddAppearanceMenu()
 _pool:RefreshIndex()
 
 
