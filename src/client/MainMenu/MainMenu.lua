b40a605a52fbadd718e8d8eba08e5c9938d768aa
Ray <raymodsgames@gmail.com>
2018-11-29 22:50:49 -0800

    add teleport to mugshot room on create or edit

--- src/client/MainMenu/MainMenu.lua
+++ src/client/MainMenu/MainMenu.lua
@@ -1,12 +1,13 @@
 local COORDS = {
-
-  LSIA = { -1031.64, -2742.02, 12.2, 350.8 }
+  LSIA = { -1029.84, -2746.72, 12.2, 350.8 },
+  MUG = { 405.621, -997.099365, -100.0, 85.0 },
 }
 
 local _editItem = NativeUI.CreateItem('Edit Character', 'Edit your current character.')
 local _initItem = NativeUI.CreateItem('Create Character', 'Create a brand new character.')
 local _leaveItem = NativeUI.CreateItem('Leave Character Creation', 'Exit character creation menu and spawn at LSIA')
 local _menu = NativeUI.CreateMenu('Character Management')
+
 
 -- TODO: Move to utils
 local function PhaseEntity(entity)
@@ -36,9 +37,24 @@ local function finish()
   PhaseEntity(PlayerPedId())
 end
 
+local function tpMugshot()
+  DoScreenFadeOut(300)
+  Citizen.Wait(300)
+  StartPlayerTeleport(PlayerId(), table.unpack(COORDS.MUG))
+
+  while (IsPlayerTeleportActive()) do
+    Citizen.Wait(50)
+  end
+
+  Citizen.Wait(300)
+  DoScreenFadeIn(2500)
+end
+
 local function handleSelectOption(sender, item, index)
   if (item == _leaveItem) then
     finish()
+  elseif (item == _initItem or item == _editItem) then
+    tpMugshot()
   end
 end
 
